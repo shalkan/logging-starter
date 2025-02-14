3ad8e392171353f5cc2d575e4a3e77a007754736
r.gilfanov <gilfanov@cg.ru>
2025-02-14 11:05:26 +0300

    initial commit

---
+++ src/main/java/ru/shalkan/LoggingStarterAutoConfiguration.java
@@ -0,0 +1,8 @@
+package ru.shalkan;
+
+public class LoggingStarterAutoConfiguration {
+
+    public static void println(String input) {
+        System.out.println("Print from logging-starter:" + input);
+    }
+}
